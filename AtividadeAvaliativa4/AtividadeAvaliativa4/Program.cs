a94c3e7cc3d67e85c28041035f99fabe55a9f8a2
Luiz Dias <luizhsouzadias@gmail.com>
2023-03-14 18:52:30 -0300

    Add files via upload

---
+++ AtividadeAvaliativa4/AtividadeAvaliativa4/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtividadeAvaliativa4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string mensagem;
+            bool repetir = true;
+            Triangulo triangulo = new Triangulo();
+
+            while (repetir)
+            {
+                Console.WriteLine("Digite o tamanho dos lados A, B e C do trinângulo");
+                triangulo.setladoA(double.Parse(Console.ReadLine()));
+                triangulo.setladoB(double.Parse(Console.ReadLine()));
+                triangulo.setladoC(double.Parse(Console.ReadLine()));
+
+                //Console.WriteLine("" + triangulo.getladoA() + triangulo.getladoB() + triangulo.getladoC()+ ""); testar set e get
+
+                if(!verificaVazio(triangulo.getladoA(), triangulo.getladoB(), triangulo.getladoC()))
+                {
+                    mensagem = triangulo.verificaTriangulo();
+
+                    Console.WriteLine(mensagem);
+
+                    repetir = false;
+                }
+                else
+                {
+                    Console.WriteLine("\nDigite novamente os números, um dos lados digitados estão inválidos");
+                    Console.WriteLine("");
+                }
+
+            }
+
+        }
+
+        static bool verificaVazio(double ladoA, double ladoB, double ladoC)
+        {
+            if (ladoA <= 0 || ladoB <= 0 || ladoC <=0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ AtividadeAvaliativa4/AtividadeAvaliativa4/Triangulo.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtividadeAvaliativa4
+{
+    internal class Triangulo
+    {
+        private double ladoA;
+        private double ladoB;
+        private double ladoC;
+
+        public Triangulo() { }
+        public Triangulo(double ladoA, double ladoB, double ladoC)
+        {
+            this.ladoA = ladoA;
+            this.ladoB = ladoB;
+            this.ladoC = ladoC;
+        }
+
+        public double getladoA()
+        {
+            return this.ladoA;
+        }
+        public double getladoB()
+        {
+            return this.ladoB;
+        }
+        public double getladoC()
+        {
+            return this.ladoC;
+        }
+
+        public void setladoA(double ladoA)
+        {
+            this.ladoA = ladoA;
+        }
+        public void setladoB(double ladoB)
+        {
+            this.ladoB = ladoB;
+        }
+        public void setladoC(double ladoC)
+        {
+            this.ladoC = ladoC;
+        }
+        public string verificaTriangulo()
+        {
+            string mensagem;
+            double ladoA, ladoB, ladoC;
+
+            mensagem = "";
+
+            ladoA = this.ladoA;
+            ladoB = this.ladoB;
+            ladoC = this.ladoC;
+
+            if (ladoA >= ladoB + ladoC)
+            {
+                mensagem = "Não forma triângulo";
+            }
+            else if (ladoA == ladoB && ladoB == ladoC)
+            {
+                mensagem = "Trinângulo Equilátero";
+            }
+            else if ((ladoA * ladoA) == (ladoB * ladoB) + (ladoC * ladoC))
+            {
+                mensagem = "Triângulo Retângulo";
+            }
+            else if ((ladoA * ladoA) > (ladoB * ladoB) + (ladoC * ladoC))
+            {
+                mensagem = "Trinângulo Obstusângulo";
+            }
+            else if ((ladoA * ladoA) < (ladoB * ladoB) + (ladoC * ladoC))
+            {
+                mensagem = "Trinângulo Acutâno";
+            }else if (ladoA == ladoB || ladoA == ladoC || ladoB == ladoC)
+            {
+                mensagem = "Trinângulo Isósceles";
+            }
+
+            return mensagem;
+        }
+
+
+    }
+}
